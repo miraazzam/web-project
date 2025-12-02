b0244c6e7ad543e7b1216df0ae618cc6f96660ff
miraazzam <mira.azzam137@gmail.com>
2025-12-02 18:38:01 +0200

    Refactor order.js to enhance dropdown functionality

--- script/order.js
+++ script/order.js
@@ -1,4 +1,43 @@
-$(document).ready(function () {
+let hamburgerData = {
+    links: [
+        { selector: ".page a.home" },
+        { selector: ".page a.menu" },
+        { selector: ".page a.about" },
+        { selector: ".login a.login" },
+        { selector: ".login a.signup" }
+    ]
+};
+
+
+$(function () {
+  let $hamburger = $(".hamburger");
+  let $header = $(".header");
+  
+  let $dropdown = $("<div class='dropdown'></div>");
+  $header.append($dropdown);
+
+  $(".page a, .login a").each(function () {
+    $dropdown.append($(this).clone());
+  });
+
+  $hamburger.on("click", function (e) {
+    e.stopPropagation();
+    $dropdown.toggleClass("show");
+  });
+
+  $dropdown.find("a").on("click", function () {
+    $dropdown.removeClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$header.is(e.target) && $header.has(e.target).length === 0) {
+      $dropdown.removeClass("show");
+    }
+  });
+});
+
+
+$(function () {
 
     let total = 0;
 
@@ -29,5 +68,11 @@ $(document).ready(function () {
 
         $(this).parent().remove();
     });
+
+   
+$("#btn-cash").on("click", function() {
+    alert("Please login to continue with Cash on Delivery.");
+});
+
 
 });

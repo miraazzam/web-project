e9c95afaf1a51b8d9d33717941f93cee136f3e72
miraazzam <mira.azzam137@gmail.com>
2025-12-02 16:35:12 +0200

    Update ending.js

--- script/ending.js
+++ script/ending.js
@@ -1,4 +1,44 @@
-$(document).ready(function() {
+let hamburgerData = {
+    links: [
+        { selector: ".page a.home" },
+        { selector: ".page a.menu" },
+        { selector: ".page a.about" },
+        { selector: ".login a.login" },
+        { selector: ".login a.signup" }
+    ]
+};
+
+
+$(function () {
+  let $hamburger = $(".hamburger");
+  let $header = $(".header");
+  
+  let $dropdown = $("<div class='dropdown'></div>");
+  $header.append($dropdown);
+
+  $(".page a, .login a").each(function () {
+    $dropdown.append($(this).clone());
+  });
+
+  $hamburger.on("click", function (e) {
+    e.stopPropagation();
+    $dropdown.toggleClass("show");
+  });
+
+  $dropdown.find("a").on("click", function () {
+    $dropdown.removeClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$header.is(e.target) && $header.has(e.target).length === 0) {
+      $dropdown.removeClass("show");
+    }
+  });
+});
+
+
+
+$(function() {
 
   $(".feedback, #map").hide().fadeIn(1200);
   $(".feedback button").on("click", function() {
@@ -23,3 +63,4 @@ document.getElementById("coffeeQuote").textContent =
 
 
 });
+

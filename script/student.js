5a3fb96bd29748f0481c008ffa095bebc78abfa8
miraazzam <mira.azzam137@gmail.com>
2025-12-02 16:35:40 +0200

    Refactor student login and add hamburger menu

--- script/student.js
+++ script/student.js
@@ -1,3 +1,42 @@
+let hamburgerData = {
+    links: [
+        { selector: ".page a.home" },
+        { selector: ".page a.menu" },
+        { selector: ".page a.about" },
+        { selector: ".login a.login" },
+        { selector: ".login a.signup" }
+    ]
+};
+
+
+$(function () {
+  let $hamburger = $(".hamburger");
+  let $header = $(".header");
+  
+  let $dropdown = $("<div class='dropdown'></div>");
+  $header.append($dropdown);
+
+  $(".page a, .login a").each(function () {
+    $dropdown.append($(this).clone());
+  });
+
+  $hamburger.on("click", function (e) {
+    e.stopPropagation();
+    $dropdown.toggleClass("show");
+  });
+
+  $dropdown.find("a").on("click", function () {
+    $dropdown.removeClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$header.is(e.target) && $header.has(e.target).length === 0) {
+      $dropdown.removeClass("show");
+    }
+  });
+});
+
+
 $("#loginBtn").click(function () {
     $("#studentLoginForm").addClass("pop");
 });
@@ -20,6 +59,8 @@ $("#studentLoginForm").submit(function (e) {
         $("#loginMessage").css("color", "red").text("Invalid login.");
     }
 });
+
+
 
 
 

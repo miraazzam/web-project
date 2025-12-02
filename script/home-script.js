01099d553cc117b8c04e39b4ec5ca73d3aa80bc7
miraazzam <mira.azzam137@gmail.com>
2025-12-02 09:36:50 +0200

    Add files via upload

---
+++ script/home-script.js
@@ -0,0 +1,31 @@
+let hamburger = document.getElementById('hamburger');  
+let header    = document.querySelector('.header');     
+
+let dropdown = document.createElement('div');
+dropdown.classList.add('dropdown');
+header.appendChild(dropdown);
+
+
+let allLinks = document.querySelectorAll('.page a, .login a');
+allLinks.forEach(function(link) {
+  dropdown.appendChild(link.cloneNode(true));
+});
+
+hamburger.onclick = function () {
+  dropdown.classList.toggle('show');                  
+};
+
+
+let dropdownLinks = dropdown.querySelectorAll('a');
+dropdownLinks.forEach(function(link) {
+  link.onclick = function () {
+    dropdown.classList.remove('show');
+  };
+});
+
+
+document.onclick = function (event) {
+  if (!header.contains(event.target)) {
+    dropdown.classList.remove('show');
+  }
+};

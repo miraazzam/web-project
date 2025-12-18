dc8c380d1442584ad2b2f69b41710bf4c4ba77ce
miraazzam <mira.azzam137@gmail.com>
2025-12-18 14:39:29 +0200

    Update order.js

--- script/order.js
+++ script/order.js
@@ -1,3 +1,43 @@
+let hamburgerData = {
+    links: [
+        { selector: ".page a.home" },
+        { selector: ".page a.menu" },
+        { selector: ".page a.about" },
+        { selector: ".login a.login" },
+        { selector: ".login a.signup" }
+    ]
+};
+
+
+
+$(function () {
+  let $hamburger = $(".hamburger");
+  let $header = $(".header");
+  
+  let $dropdown = $("<div class='dropdown'></div>");
+  $header.append($dropdown);
+
+  $(".page a, .login a").each(function () {
+    $dropdown.append($(this).clone());
+  });
+
+  $hamburger.on("click", function (e) {
+    e.stopPropagation();
+    $dropdown.toggleClass("show");
+  });
+
+  $dropdown.find("a").on("click", function () {
+    $dropdown.removeClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$header.is(e.target) && $header.has(e.target).length === 0) {
+      $dropdown.removeClass("show");
+    }
+  });
+});
+
+
 $(document).ready(function () {
 
     let total = 0;

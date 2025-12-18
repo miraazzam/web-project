e73f4a981a70f892bb23af1f101248515e582372
Kinda-A <affarakn@students.rhu.edu.lb>
2025-12-18 11:04:39 +0200

    Add files via upload

---
+++ script/loginedited.js
@@ -0,0 +1,72 @@
+let hamburgerData = {
+    links: [
+        { selector: ".page a.home" },
+        { selector: ".page a.menu" },
+        { selector: ".page a.about" },
+        { selector: ".login a.login" },
+        { selector: ".login a.signup" }
+    ]
+};
+
+$(function () {
+  let $hamburger = $(".hamburger");
+  let $header = $(".header");
+  
+  let $dropdown = $("<div class='dropdown'></div>");
+  $header.append($dropdown);
+
+  $(".page a, .login a").each(function () {
+    $dropdown.append($(this).clone());
+  });
+
+  $hamburger.on("click", function (e) {
+    e.stopPropagation();
+    $dropdown.toggleClass("show");
+  });
+
+  $dropdown.find("a").on("click", function () {
+    $dropdown.removeClass("show");
+  });
+
+  $(document).on("click", function (e) {
+    if (!$header.is(e.target) && $header.has(e.target).length === 0) {
+      $dropdown.removeClass("show");
+    }
+  });
+});
+
+
+
+
+$("#cancelBtn").click(function () {
+   $("#loginMessage").text("");
+   $("#studentEmail").val("");
+   $("#studentPassword").val("");
+   $(".offers").hide()
+});
+
+$("#studentLoginForm").submit(function (e) {
+    e.preventDefault();  
+    const email = $("#studentEmail").val();
+    const pass = $("#studentPassword").val();
+
+    if ((email.endsWith(".edu.lb") || email.endsWith(".edu")) && pass === "123456") {
+        $("#loginMessage").css("color", "green").text("Login successful! Discounts unlocked.");
+        $(".discountPrice").show();
+       
+        $(".offers").show();
+
+    } else {
+        $("#loginMessage").css("color", "red").text("Invalid login.");
+    }
+});
+
+
+
+
+
+
+
+
+
+
